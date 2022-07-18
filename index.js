043d17e7abc9a43f0495a4c480c4d80b076b4ebf
Tanvir Hossain Sadi <htanvir.sadi@gmail.com>
2022-07-18 22:05:23 +0600

    fixed socket server issues

--- index.js
+++ index.js
@@ -2,12 +2,25 @@ const express = require('express')
 require('dotenv').config()
 const app = express()
 const cors = require('cors')
+const http = require('http')
+
+// create server
 const port = process.env.PORT || 5000
+const server = http.createServer(app)
+const io = require("socket.io")(server, {
+    cors: {
+        origin: port,
+        methods: ["GET", "POST"],
+        allowedHeaders: ["my-custom-header"],
+        credentials: true
+    }
+});
 
 // components
 require('./config/db')
 const userRoutes = require('./routes/userRoutes')
 const messageRoutes = require('./routes/messageRoutes')
+
 
 // middleware
 app.use(cors())
@@ -20,8 +33,56 @@ app.use(express.static('public'))
 app.use(userRoutes)
 app.use(messageRoutes)
 
+var users = []
+
+function addUser(userId, socketId, userInfo) {
+    const isUserExist = users.some(user => user.userId === userId)
+    if (!isUserExist) {
+        console.log(`this is active`);
+        users.push({ userId, socketId, userInfo })
+    }
+}
+
+const removeUser = (socketId) => {
+    users = users.filter(user => user.socketId !== socketId)
+}
+
+const findUser = (id) => {
+    const ss = users.find(user => user.userId === id)
+    return ss
+
+}
+
+io.on('connection', (socket) => {
+    console.log(`socket is running on port ${port}`);
+
+    socket.on('addUser', (userId, userInfo) => {
+        addUser(userId, socket.id, userInfo)
+        io.emit('getUsers', users)
+    })
+
+    socket.on('disconnect', () => {
+        removeUser(socket.id)
+        io.emit('getUsers', users)
+    })
 
 
-app.listen(port, () => {
+    socket.on('sendMessage', (messageInfo) => {
+        // console.log(messageInfo);
+        const isUserActive = findUser(messageInfo.receiverId)
+        io.emit('receiveMessage', messageInfo)
+    })
+
+
+    socket.on('typingMessage', (data) => {
+        const isUserActive = findUser(data.receiverId)
+        socket.broadcast.emit('typingMessageStatus', data)
+    })
+
+
+})
+
+
+server.listen(port, () => {
     console.log(`server is listening on port ${port}`);
 })
