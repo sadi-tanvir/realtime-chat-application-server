74d5aa86be97ef1a6986f1e12fd9324d8e73edb9
Tanvir Hossain Sadi <htanvir.sadi@gmail.com>
2022-07-18 22:23:44 +0600

    connected with mongodb atlas

--- config/db.js
+++ config/db.js
@@ -1,12 +1,12 @@
 const mongoose = require('mongoose')
 
 
-
-mongoose.connect(process.env.URL, {
+const Url = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@cluster0.jnizw.mongodb.net/${process.env.MONGO_USER}?retryWrites=true&w=majority`;
+mongoose.connect(Url, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 }).then(() => console.log(`MongoDB Connected`))
-.catch(error => {
-    console.log(error)
-    process.exit()
-})
+    .catch(error => {
+        console.log(error)
+        process.exit()
+    })
